fefef2fde844eb315bdad6dd4e82e0a2b79da3aa
Thiago Marquezini <42454145+thiago-marquezini@users.noreply.github.com>
2021-11-17 20:38:53 -0300

    updated directories

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ namespace InoaTest_Console
                 {
                     for (int I = 0; I < (args.Length / SymbolArgCount); I++)
                     {
-                        int SymbolIndex  = I * SymbolArgCount;
+                        int SymbolIndex = I * SymbolArgCount;
 
-                        string Symbol    = args[SymbolIndex];
-                        double RefSell   = double.Parse(args[SymbolIndex + 1], System.Globalization.CultureInfo.InvariantCulture);
-                        double RefBuy    = double.Parse(args[SymbolIndex + 2], System.Globalization.CultureInfo.InvariantCulture);
+                        string Symbol = args[SymbolIndex];
+                        double RefSell = double.Parse(args[SymbolIndex + 1], System.Globalization.CultureInfo.InvariantCulture);
+                        double RefBuy = double.Parse(args[SymbolIndex + 2], System.Globalization.CultureInfo.InvariantCulture);
 
                         B3AtivosMonitor.AddSymbol(Symbol, RefSell, RefBuy);
                     }
@@ -27,7 +27,8 @@ namespace InoaTest_Console
 
                 B3AtivosMonitor.Run();
 
-            } catch
+            }
+            catch
             {
                 Console.WriteLine("[ ERRO ] Argumentos invalidos! Uso: InoaTest_Console.exe [ATIVO0 VENDA0 COMPRA0 ATIVO1 VENDA1 COMPRA1 ...]");
                 Environment.Exit(0);
